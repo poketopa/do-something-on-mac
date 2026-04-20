9498c62868aed701d5dff17cd82995958483be72
poketopa <lhs5427ll@gmail.com>
2026-04-20 23:46:15 +0900

    feat: start project

---
+++ DoSomething/DoSomethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoSomethingApp.swift
+//  DoSomething
+//
+//  Created by 임현성 on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct DoSomethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
